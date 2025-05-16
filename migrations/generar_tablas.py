80d393c22b97289ce3e642ffd7d920add6b901e4
BarbDMC <barbaradmorantesc@gmail.com>
2025-05-16 14:29:54 -0500

    fix: os and paths imports fixed

--- migrations/generar_tablas.py
+++ migrations/generar_tablas.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from SQLAlchemy.db import engine, Base
 import models  # importante: importa __init__.py que carga los modelos
 
